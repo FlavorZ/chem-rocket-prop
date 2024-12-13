76993728f7954f463244a8992b730723624e6347
FlavorZ <christian.kolset@gmail.com>
2024-12-12 21:16:20 -0700

    Updated model source file

--- FinalProject/Data/dataProcessing.m
+++ FinalProject/Data/dataProcessing.m
@@ -4,7 +4,7 @@ team3 = lvm_import('Team3.lvm')
 team4 = lvm_import('Team4.lvm')
 team5 = lvm_import('Team5.lvm')
 
-modelData = readtable('./modelData.csv');
+modelData = readtable('Data/CleanedModel.xlsx');
 
 data1 = team1.Segment1.data;
 data2 = team2.Segment1.data;
