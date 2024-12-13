864f3bab73e8da10abbcdd11da579cd3174610b1
FlavorZ <christian.kolset@gmail.com>
2024-12-13 02:08:53 -0700

    Implemented easy to switch teams function

--- FinalProject/plotFigure.m
+++ FinalProject/plotFigure.m
@@ -1,17 +1,25 @@
-reload = false;
+reload = true;
+
 if reload == true
     clear all
     clc
 
+    % Team number - 1-5
+    teamnum = 1;
+
     % Load Data
-    team1 = lvm_import('Team1.lvm', 0)
+    file = "Data/Team" + string(teamnum) + ".lvm";
+    team = lvm_import("Data/Team" + string(teamnum) + ".lvm", 0);
     modelData = readtable('Data/CleanedModel.xlsx'); % Load simulated data
-    data = team1.Segment1.data; % Load Experimental Data
+    data = team.Segment1.data; % Load Experimental Data
 end
+
+g_c = 32.474; % ft/s^2
+time_frames = [283.175 12.5; 30 12.5; 27.5 14; 166 14; 53.3 14];
 
 % Import Experimental Data
 time = data(:, 1);
-mass_flow = data(:, 4); 
+mass_ox_flow = data(:, 4); 
 pressure = data(:, 5);
 force = data(:, 8);
 master_setpoint = data(:, 9);
@@ -26,31 +34,31 @@ isp_model = table2array(modelData(:,11));
 Cstar_model = table2array(modelData(:,5));
 OF_model = table2array(modelData(:,3));
 
-% Model Offset
-model_time_offset = 252.175+31;
-%time_model = time_model + model_time_offset; % Apply time offset
-time = time - model_time_offset;
-
 % Experimental Data Calculated
 A_th = 7.782e-4 + 0.491; % function for throat area w.r.t. time
-isp = force ./ mass_flow; % Eqn 2.29
-Cstar = pressure * A_th ./ (mass_flow* 4.4945e-5); % Eqn 2.26 % convert SLPM to lb/s
+isp = force ./ (mass_ox_flow * g_c); % Eqn 2.29
+Cstar = pressure * A_th ./ (mass_ox_flow* 4.4945e-5); % Eqn 2.26 % convert SLPM to lb/s
+
+% Model Offset
+experiment_start_time = time_frames(teamnum,1);
+experiment_duration = time_frames(teamnum,2);
+time = time - experiment_start_time;
 
 % Create a figure with 2x2 subplots
 figure;
 
 % Plot 1: Flowrate vs Time
 subplot(3, 2, 1);
-plot(time, mass_flow, 'b:', 'LineWidth', 1.5); % Experimental dotted line
+plot(time, mass_ox_flow, 'b:', 'LineWidth', 1.5); % Experimental dotted line
 hold on;
 plot(time_model, mass_flow_model, 'r-', 'LineWidth', 1.5); % Model solid line
-plot(time, master_setpoint, 'g:', 'LineWidth', 1.0); % Experimental dotted line
+plot(time, master_setpoint, 'g:', 'LineWidth', 1.5); % Experimental dotted line
 xlabel('Time (s)');
 ylabel('m_d_o_t (SLPM)');
 title('Flowrate vs Time');
 legend('Experimental', 'Model', 'Master Setpoint', 'Location', 'best');
-xlim([0, 12.5]);
-ylim([0, max(max(mass_flow), max(mass_flow_model)) * 1.2]); % Adjust Y-axis
+xlim([0, experiment_duration]);
+ylim([0, max(max(mass_ox_flow), max(mass_flow_model)) * 1.2]); % Adjust Y-axis
 grid on;
 hold off;
 
@@ -63,7 +71,7 @@ xlabel('Time (s)');
 ylabel('Pressure (psi)');
 title('Pressure vs Time');
 legend('Experimental', 'Model', 'Location', 'best');
-xlim([0, 12.5]);
+xlim([0, experiment_duration]);
 ylim([0, max(max(pressure), max(pressure_model)) * 1.2]); % Adjust Y-axis
 grid on;
 hold off;
@@ -77,7 +85,7 @@ xlabel('Time (s)');
 ylabel('Force (lbf)');
 title('Force vs Time');
 legend('Experimental', 'Model', 'Location', 'best');
-xlim([0, 12.5]);
+xlim([0, experiment_duration]);
 ylim([min(force)*1.2, max(max(force), max(force_model)) * 1.2]); % Adjust Y-axis
 grid on;
 hold off;
@@ -90,7 +98,7 @@ plot(time_model, isp_model, 'r', 'LineWidth', 1.5); % Model solid line
 xlabel('Time (s)');
 ylabel('Isp (s)');
 title('Isp vs Time');
-xlim([0, 12.5]);
+xlim([0, experiment_duration]);
 ylim([0, max(isp) * 1.2]);
 grid on;
 hold off;
@@ -103,10 +111,9 @@ plot(time_model, Cstar_model, 'r', 'LineWidth', 1.5); % Model solid line
 xlabel('Time (s)');
 ylabel('Cstar (in/s)');
 title('Cstar vs Time');
-xlim([0, 12.5]);
+xlim([0, experiment_duration]);
 ylim([0, max(Cstar) * 1.2]);
 grid on;
 hold off;
 
-% Adjust layout
-sgtitle('Rocket Parameters vs Time');
+sgtitle("Team "+ string(teamnum))
