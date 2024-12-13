39ea9cc42f8d20c1c7d11ec5c47083802c910f45
FlavorZ <christian.kolset@gmail.com>
2024-12-13 00:59:11 -0700

    Added loading of data directly from script

--- FinalProject/plotFigure.m
+++ FinalProject/plotFigure.m
@@ -1,18 +1,25 @@
-% Experimental Data
-data = data1;
+reload = false;
+if reload == true
+    clear all
+    clc
+
+    % Load Data
+    team1 = lvm_import('Team1.lvm', 0)
+    modelData = readtable('Data/CleanedModel.xlsx'); % Load simulated data
+    data = team1.Segment1.data; % Load Experimental Data
+end
 
 % Import Experimental Data
 time = data(:, 1);
-mass_flow = data(:, 4);
+mass_flow = data(:, 4); 
 pressure = data(:, 5);
 force = data(:, 8);
 master_setpoint = data(:, 9);
-Cstar = 165 * A(t) / mass_flow;
 
 % Import Model Data
 time_model = table2array(modelData(:,1));
-mass_flow_ox_model = table2array(modelData(:,2)) * 22045.89; % Convert m_dot_ox to SLPM
-mass_flow_model = mass_flow_ox_model + 500; %SLPM
+mass_flow_f_model = table2array(modelData(:,2)) * 22045.89; % Convert m_dot_ox to SLPM
+mass_flow_model = mass_flow_f_model + 500; %SLPM
 pressure_model = table2array(modelData(:,7));
 force_model = table2array(modelData(:,10));
 isp_model = table2array(modelData(:,11));
@@ -20,15 +27,20 @@ Cstar_model = table2array(modelData(:,5));
 OF_model = table2array(modelData(:,3));
 
 % Model Offset
-model_time_offset = 30-1.3+1.9;
+model_time_offset = 252.175+31;
 %time_model = time_model + model_time_offset; % Apply time offset
 time = time - model_time_offset;
+
+% Experimental Data Calculated
+A_th = 7.782e-4 + 0.491; % function for throat area w.r.t. time
+isp = force ./ mass_flow; % Eqn 2.29
+Cstar = pressure * A_th ./ (mass_flow* 4.4945e-5); % Eqn 2.26 % convert SLPM to lb/s
 
 % Create a figure with 2x2 subplots
 figure;
 
 % Plot 1: Flowrate vs Time
-subplot(2, 2, 1);
+subplot(3, 2, 1);
 plot(time, mass_flow, 'b:', 'LineWidth', 1.5); % Experimental dotted line
 hold on;
 plot(time_model, mass_flow_model, 'r-', 'LineWidth', 1.5); % Model solid line
@@ -37,13 +49,13 @@ xlabel('Time (s)');
 ylabel('m_d_o_t (SLPM)');
 title('Flowrate vs Time');
 legend('Experimental', 'Model', 'Master Setpoint', 'Location', 'best');
-xlim([0, 12]);
+xlim([0, 12.5]);
 ylim([0, max(max(mass_flow), max(mass_flow_model)) * 1.2]); % Adjust Y-axis
 grid on;
 hold off;
 
 % Plot 2: Pressure vs Time
-subplot(2, 2, 2);
+subplot(3, 2, 2);
 plot(time, pressure, 'b:', 'LineWidth', 1.5); % Experimental dotted line
 hold on;
 plot(time_model, pressure_model, 'r-', 'LineWidth', 1.5); % Model solid line
@@ -51,13 +63,13 @@ xlabel('Time (s)');
 ylabel('Pressure (psi)');
 title('Pressure vs Time');
 legend('Experimental', 'Model', 'Location', 'best');
-xlim([0, 12]);
+xlim([0, 12.5]);
 ylim([0, max(max(pressure), max(pressure_model)) * 1.2]); % Adjust Y-axis
 grid on;
 hold off;
 
 % Plot 3: Force vs Time
-subplot(2, 2, 3);
+subplot(3, 2, 3);
 plot(time, force, 'b:', 'LineWidth', 1.5); % Experimental dotted line
 hold on;
 plot(time_model, force_model, 'r-', 'LineWidth', 1.5); % Model solid line
@@ -65,19 +77,34 @@ xlabel('Time (s)');
 ylabel('Force (lbf)');
 title('Force vs Time');
 legend('Experimental', 'Model', 'Location', 'best');
-xlim([0, 12]);
-ylim([0, max(max(force), max(force_model)) * 1.2]); % Adjust Y-axis
+xlim([0, 12.5]);
+ylim([min(force)*1.2, max(max(force), max(force_model)) * 1.2]); % Adjust Y-axis
 grid on;
 hold off;
 
 % Plot 4: Isp vs Time
-subplot(2, 2, 4);
-plot(time, master_setpoint, 'b:', 'LineWidth', 1.5); % Experimental dotted line
+subplot(3, 2, 4);
+plot(time, isp, 'b:', 'LineWidth', 1.5); % Experimental dotted line
+hold on;
+plot(time_model, isp_model, 'r', 'LineWidth', 1.5); % Model solid line
 xlabel('Time (s)');
-ylabel('Master Setpoint');
-title('Master Setpoint vs Time');
-xlim([0, 12]);
-ylim([0, max(master_setpoint) * 1.2]); % Adjust Y-axis
+ylabel('Isp (s)');
+title('Isp vs Time');
+xlim([0, 12.5]);
+ylim([0, max(isp) * 1.2]);
+grid on;
+hold off;
+
+% Plot 4: Cstar vs Time
+subplot(3, 2, 5);
+plot(time, Cstar, 'b:', 'LineWidth', 1.5); % Experimental dotted line
+hold on;
+plot(time_model, Cstar_model, 'r', 'LineWidth', 1.5); % Model solid line
+xlabel('Time (s)');
+ylabel('Cstar (in/s)');
+title('Cstar vs Time');
+xlim([0, 12.5]);
+ylim([0, max(Cstar) * 1.2]);
 grid on;
 hold off;
 
