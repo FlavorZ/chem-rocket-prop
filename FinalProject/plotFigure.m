4ca3c32b3e3035292792c54eceaf3db915039cb4
FlavorZ <christian.kolset@gmail.com>
2024-12-12 23:34:36 -0700

    Fixed figures and added equations

--- FinalProject/plotFigure.m
+++ FinalProject/plotFigure.m
@@ -1,56 +1,85 @@
+% Experimental Data
+data = data1;
+
 % Import Experimental Data
-time = data1(:, 1); % First column is assumed to be time
-mass_flow_ox = data1(:, 4); % Example data for plotting
-pressure = data1(:, 5); % Example data for plotting
-force = data1(:, 8); % Example data for plotting
-master_setpoint = data1(:, 9); % Example data for plotting
+time = data(:, 1);
+mass_flow = data(:, 4);
+pressure = data(:, 5);
+force = data(:, 8);
+master_setpoint = data(:, 9);
+Cstar = 165 * A(t) / mass_flow;
 
 % Import Model Data
-time_model = modelData(:,1);
-mass_flow_ox_model = modelData(:,'MassFlowRateFuel_lb_s_');
-pressure_model = modelData(:,'ChamberPressure_psi_');
-force_model = modelData(:, 'Thrust_lbf_');
+time_model = table2array(modelData(:,1));
+mass_flow_ox_model = table2array(modelData(:,2)) * 22045.89; % Convert m_dot_ox to SLPM
+mass_flow_model = mass_flow_ox_model + 500; %SLPM
+pressure_model = table2array(modelData(:,7));
+force_model = table2array(modelData(:,10));
+isp_model = table2array(modelData(:,11));
+Cstar_model = table2array(modelData(:,5));
+OF_model = table2array(modelData(:,3));
 
 % Model Offset
-model_time_offset = 30;
-time_model = table2array(time_model);
-time_model = time_model + model_time_offset;
-time_model = array2table(time_model);
+model_time_offset = 30-1.3+1.9;
+%time_model = time_model + model_time_offset; % Apply time offset
+time = time - model_time_offset;
 
 % Create a figure with 2x2 subplots
 figure;
 
 % Plot 1: Flowrate vs Time
 subplot(2, 2, 1);
-plot(time, mass_flow_ox, 'b-', 'LineWidth', 1.5);
+plot(time, mass_flow, 'b:', 'LineWidth', 1.5); % Experimental dotted line
+hold on;
+plot(time_model, mass_flow_model, 'r-', 'LineWidth', 1.5); % Model solid line
+plot(time, master_setpoint, 'g:', 'LineWidth', 1.0); % Experimental dotted line
 xlabel('Time (s)');
-ylabel('m_dot_ox');
+ylabel('m_d_o_t (SLPM)');
 title('Flowrate vs Time');
+legend('Experimental', 'Model', 'Master Setpoint', 'Location', 'best');
+xlim([0, 12]);
+ylim([0, max(max(mass_flow), max(mass_flow_model)) * 1.2]); % Adjust Y-axis
 grid on;
+hold off;
 
 % Plot 2: Pressure vs Time
 subplot(2, 2, 2);
-plot(time, pressure, 'r--', 'LineWidth', 1.5);
+plot(time, pressure, 'b:', 'LineWidth', 1.5); % Experimental dotted line
+hold on;
+plot(time_model, pressure_model, 'r-', 'LineWidth', 1.5); % Model solid line
 xlabel('Time (s)');
-ylabel('Pressure');
+ylabel('Pressure (psi)');
 title('Pressure vs Time');
+legend('Experimental', 'Model', 'Location', 'best');
+xlim([0, 12]);
+ylim([0, max(max(pressure), max(pressure_model)) * 1.2]); % Adjust Y-axis
 grid on;
+hold off;
 
 % Plot 3: Force vs Time
 subplot(2, 2, 3);
-plot(time, force, 'g-.', 'LineWidth', 1.5);
+plot(time, force, 'b:', 'LineWidth', 1.5); % Experimental dotted line
+hold on;
+plot(time_model, force_model, 'r-', 'LineWidth', 1.5); % Model solid line
 xlabel('Time (s)');
-ylabel('Force');
+ylabel('Force (lbf)');
 title('Force vs Time');
+legend('Experimental', 'Model', 'Location', 'best');
+xlim([0, 12]);
+ylim([0, max(max(force), max(force_model)) * 1.2]); % Adjust Y-axis
 grid on;
+hold off;
 
-% Plot 4: Flowrate Setpoint vs Time
+% Plot 4: Isp vs Time
 subplot(2, 2, 4);
-plot(time, master_setpoint, 'k:', 'LineWidth', 1.5);
+plot(time, master_setpoint, 'b:', 'LineWidth', 1.5); % Experimental dotted line
 xlabel('Time (s)');
-ylabel('Master setpoint');
-title('Master setpoint vs Time');
+ylabel('Master Setpoint');
+title('Master Setpoint vs Time');
+xlim([0, 12]);
+ylim([0, max(master_setpoint) * 1.2]); % Adjust Y-axis
 grid on;
+hold off;
 
 % Adjust layout
 sgtitle('Rocket Parameters vs Time');
