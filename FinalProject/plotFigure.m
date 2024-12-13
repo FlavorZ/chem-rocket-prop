e6de16782089a4e320bd58dda97e64170f0753a0
FlavorZ <christian.kolset@gmail.com>
2024-12-12 20:35:19 -0700

    Added experimental data to plots

--- FinalProject/plotFigure.m
+++ FinalProject/plotFigure.m
@@ -1,9 +1,20 @@
-% Assuming 'data1' is the dataset
+% Import Experimental Data
 time = data1(:, 1); % First column is assumed to be time
 mass_flow_ox = data1(:, 4); % Example data for plotting
 pressure = data1(:, 5); % Example data for plotting
 force = data1(:, 8); % Example data for plotting
 master_setpoint = data1(:, 9); % Example data for plotting
+
+% Import Model Data
+modelData = table2array(modelData)
+time_model = modelData(:,1);
+mass_flow_ox_model = modelData(:,'MassFlowRateFuel_lb_s_');
+pressure_model = modelData(:,'ChamberPressure_psi_');
+force_model = modelData(:, 'Thrust_lbf_');
+
+% Model Offset
+model_time_offset = 30;
+time_model = time_model + model_time_offset
 
 % Create a figure with 2x2 subplots
 figure;
@@ -21,7 +32,7 @@ subplot(2, 2, 2);
 plot(time, pressure, 'r--', 'LineWidth', 1.5);
 xlabel('Time (s)');
 ylabel('Pressure');
-title('Parameter 5 vs Time');
+title('Pressure vs Time');
 grid on;
 
 % Plot 3: Force vs Time
